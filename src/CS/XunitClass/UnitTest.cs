3c029b2090c0fa0e59b3a9735aebd1d7f4d6ed1b
John Landheer <jlandheer@users.noreply.github.com>
2017-01-17 14:07:13 +0100

    Usings buiten de namespace

--- src/CS/XunitClass/UnitTest.cs
+++ src/CS/XunitClass/UnitTest.cs
@@ -1,15 +1,15 @@
-﻿namespace $rootnamespace$
-{
-    using FluentAssertions;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Xunit;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
 
+namespace $rootnamespace$
+{
     public class $safeitemname$
-	{
+    {
         
-	}
+    }
 }
