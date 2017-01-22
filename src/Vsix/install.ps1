344c99ea54b5b15e972be081fb2515fa99c3c01c
John Landheer <john.landheer@gmail.com>
2017-01-22 14:20:52 +0100

    Niet meer silent

--- src/Vsix/install.ps1
+++ src/Vsix/install.ps1
@@ -1,3 +1,3 @@
 Get-ChildItem -Path  '.\' -Recurse -exclude More.Xunit.Templates.vsix,install.ps1 | Remove-Item -force -recurse
 
-& 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe' .\More.Xunit.Templates.vsix /q
+& 'C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe' .\More.Xunit.Templates.vsix
